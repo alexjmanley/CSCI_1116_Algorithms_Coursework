af6ecbbfe1b609998d5a3cc452156331efce911f
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-08-18 10:01:01 -0600

    Add files via upload

---
+++ chapter22/Exercise22_03.java
@@ -0,0 +1,35 @@
+package exercise22_3;
+
+/* Alex Manley 
+ * Aug 18, 2022
+ * 
+ * Estimate Big O time is O(n)
+ */
+
+import java.util.Scanner; 
+
+public class Exercise22_03 {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in); 
+	
+		// Prompt user 
+		System.out.println("Enter a string s1: ");
+		String s1 = input.nextLine();
+		System.out.println("Enter a string s2: ");
+		String s2 = input.nextLine();
+		
+		// find match
+		int index = -1; 
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.substring(i).length() >= s2.length() && s1.substring(i, i + s2.length()).equals(s2))
+				index = i; 
+			else 
+				index = index; 
+		}
+		if(index == -1)
+			System.out.println("No Match");
+		else
+			System.out.println("Matched at index " + index); 
+	}
+}
